f4b5be37ce81cfc89c7d2c3fb532c3e5a567ef01
Vikas Surve <VikyaSurve@users.noreply.github.com>
2022-11-23 11:59:22 +0530

    Update tap-script.sh

--- tap-script.sh
+++ tap-script.sh
@@ -112,7 +112,8 @@ pivnet login --api-token=${pivnettoken}
 pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.3.0' --product-file-id=1330470
 mkdir $HOME/tanzu-cluster-essentials
 tar -xvf tanzu-cluster-essentials-linux-amd64-1.3.0.tgz -C $HOME/tanzu-cluster-essentials
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:54bf611711923dccd7c7f10603c846782b90644d48f1cb570b43a082d18e23b9
+
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=$tanzunetusername
 export INSTALL_REGISTRY_PASSWORD=$tanzunetpassword
